43fd7c528bcda617c73bd4ea2c2b21e4835dd8bd
almumeetu <almumeetu@gmail.com>
2023-04-12 15:08:53 +0600

    problem 4 & 5 done

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Problem #1:
+// Write a JavaScript program to display the current day and time in the following format.
 var today = new Date();
 var day = today.getDay();
 var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
@@ -28,3 +30,58 @@ if (hour === 0 && prepand === ' AM ') {
     }
 }
 console.log("Current Time : " + hour + prepand + " : " + minute + " : " + second);
+
+
+// problem #2:
+// Write a JavaScript program to print the contents of the current window.
+function print_current_page() {
+    window.print();
+};
+
+
+// problem #3:
+// Write a JavaScript program to get the current date.
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth() + 1;
+var yyyy = today.getFullYear();
+if (dd < 10) {
+    dd = '0' + dd;
+}
+
+if (mm < 10) {
+    mm = '0' + mm;
+}
+today = mm + '-' + dd + '-' + yyyy;
+console.log(today);
+today = mm + '/' + dd + '/' + yyyy;
+console.log(today);
+today = dd + '-' + mm + '-' + yyyy;
+console.log(today);
+today = dd + '/' + mm + '/' + yyyy;
+console.log(today);
+
+
+// Problem #4:
+// Write a JavaScript function to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+var side1 = 5;
+var side2 = 6;
+var side3 = 7;
+var s = (side1 + side2 + side3) / 2;
+var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
+console.log(area);
+
+
+// Problem #5:
+// Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front.
+function animate_string(id) {
+    var element = document.getElementById(id);
+    var textNode = element.childNodes[0]; // assuming no other children
+    var text = textNode.data;
+
+    setInterval(function () {
+        text = text[text.length - 1] + text.substring(0, text.length - 1);
+        textNode.data = text;
+    }, 100);
+}
