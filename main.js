77ec3497c6ac88f2fcb2e53e955ccf676dede382
almumeetu <almumeetu@gmail.com>
2023-05-11 15:56:24 +0600

    problem 80 done

--- main.js
+++ main.js
@@ -1169,6 +1169,15 @@ console.log(twice3040([40, 40]));
 console.log(twice3040([20, 20]));
 console.log(twice3040([30]));
 
+// 80. Write a JavaScript program to swap the first and last elements of a given array of integers. The array length should be at least 1.  
+
+function swap(arra) {
+    [arra[0], arra[arra.length - 1]] = [arra[arra.length - 1], arra[0]];
+    return arra;
+}
+console.log(swap([1, 2, 3, 4]));
+console.log(swap([0, 2, 1]));
+console.log(swap([3]));
 
 
 
