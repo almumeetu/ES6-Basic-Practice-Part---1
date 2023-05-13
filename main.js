af7d5f88d63f5d5ff1639589fe5fb589bd86680e
almumeetu <almumeetu@gmail.com>
2023-05-13 22:50:27 +0600

    problem 86 done

--- main.js
+++ main.js
@@ -1236,14 +1236,35 @@ console.log(alphabet_char_Shift("abcdxyz"))
 
 // 85. Write a JavaScript program to divide a given array of positive integers into two parts. First element belongs to the first part, second element belongs to the second part, and third element belongs to the first part and so on. Now compute the sum of two parts and store it in an array of size two.
 
-function alternate_Sums(arr) {
-    var result = [0, 0];
-    for (var i = 0; i < arr.length; i++) {
-        if (i % 2) result[1] += arr[i];
-        else
-            result[0] += arr[i];
+// function alternate_Sums(arr) {
+//     var result = [0, 0];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (i % 2) result[1] += arr[i];
+//         else
+//             result[0] += arr[i];
+//     }
+//     return result
+// }
+
+// console.log(alternate_Sums([1, 3, 6, 2, 5, 10]))
+
+
+// 86. Write a JavaScript program to find the types of a given angle. 
+
+function angle_Type(angle) {
+    if (angle < 90) {
+        return "Acute angle.";
     }
-    return result
+    if (angle === 90) {
+        return "Right angle.";
+    }
+    if (angle < 180) {
+        return "Obtuse angle.";
+    }
+    return "Straight angle.";
 }
 
-console.log(alternate_Sums([1, 3, 6, 2, 5, 10]))
+console.log(angle_Type(47))
+console.log(angle_Type(90))
+console.log(angle_Type(145))
+console.log(angle_Type(180))
